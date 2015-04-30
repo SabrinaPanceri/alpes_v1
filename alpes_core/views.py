5624bbebd763f68df08a48c53ba2f6fcb77ab63a
Caio Vinicius D. do Prado <caiovdp@gmail.com>
2015-04-29 21:36:42 -0300

    tratamento de string

--- alpes_core/views.py
+++ alpes_core/views.py
@@ -6,15 +6,21 @@ from alpes_core.models import Tese
 from django.template import RequestContext, loader
 from django.db import connections, connection
 
-
-
+import HTMLParser
+import re
 # Create your views here.
 def home(request):
 	cursor = connection.cursor()
 	idtese = "1472"
 	cursor.execute("select `arg`.`posicionamentoinicial` AS `posicionamentoinicial`, `arg`.`argumento` AS `argumento`, `rev`.`revisao` AS `revisao`, `rep`.`replica` AS `replica`, `pos`.`posicionamentofinal` AS `posicionamentofinal` from (((`argumento` `arg` join `revisao` `rev`) join `replica` `rep`) join `posicionamento` `pos`) where ((`arg`.`tese_idtese` = " + idtese + " ) and (`rev`.`argumento_idargumento` = `arg`.`idargumento`) and (`rep`.`revisao_idrevisao` = `rev`.`idrevisao`) and (`arg`.`argumentador_idargumentador` = `pos`.`argumentador_idargumentador`) and (`arg`.`tese_idtese` = `pos`.`tese_idtese`) and (`arg`.`posicionamentoinicial` is not null) and (`arg`.`argumento` is not null) and (`rev`.`revisao` is not null) and (`rep`.`replica` is not null) and (`pos`.`posicionamentofinal` is not null))")
-	dados = cursor.fetchall()	
+	dadosSql = cursor.fetchall()	
 	
+	h = HTMLParser.HTMLParser()
+	dados = []
+
+	for d in dadosSql:
+		
+		dados.append([re.sub('<[^>]*>', '', h.unescape(d[0])),re.sub('<[^>]*>', '', h.unescape(d[1])),re.sub('<[^>]*>', '', h.unescape(d[2])),re.sub('<[^>]*>', '', h.unescape(d[3])),re.sub('<[^>]*>', '', h.unescape(d[4]))])
 		
 	context = RequestContext(request,{'teses' : Tese.objects.filter(grupo_idgrupo=1064), 'dados': dados})
 	
