d97cc6ece5817393873dac1ef4e62f962970b342
sabrinapanceri <sabrinapanceri@gmail.com>
2015-05-06 15:21:54 -0300

    testas com florestas

--- alpes_core/clusterArgFinal.py
+++ alpes_core/clusterArgFinal.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
-
+#############################################################################################################
+# Imports necessários
 import HTMLParser
-import re
+import re, nltk
 from django.db import connection
 from alpes_core.pre_text_process import removeStopWords
 from nltk.stem import RSLPStemmer
 from nltk.corpus import floresta
+from nltk.probability import FreqDist
+
+#############################################################################################################
+
+#############################################################################################################
+## Desenvolvimento da lógica de execução do Núcleo de processamento do Alpes                                #
+## Aplicação das técnicas de pré-processamento textual a fim de ajudar no processo de comparação e busca    #
+## de textos similares                                                                                      #
+## Técnicas desenvolvidas                                                                                   #
+## 1 - Case folding                                                                                         #
+## 2 - Troca de caracteres acentuados por caracteres não acentuados                                         #
+## 3 - Remoção pontuações                                                                                   #
+## 4 - Remoção de stopwords                                                                                 #
+## 5 - Stemming                                                                                             #
+## 6 - Normalização (falta desenv)                                                                          #
+#############################################################################################################
+
+##############################################################################################################
+## DESENVOLVIMENTO DA FERRAMENTA "GRUPOS DE ARGUMENTAÇÃO" - ARTIGO SBIE 2014
+## UTILIZAÇÃO DO POSICIONAMENTO FINAL PARA CRIAR GRUPOS DE ALUNOS QUE CHEGARAM A CONCLUSÕES SIMILARES
+## AO FINAL DAS INTERAÇÕES DA APDT 
+##############################################################################################################  
 
 def clusterArgFinal(idtese):
     #Variaveis e funçoes para conexação com o banco de dados do Debate de Teses
@@ -36,25 +59,7 @@ def clusterArgFinal(idtese):
     #lista com dados após a aplicação de Stemming
     st_posFinal = []
     st_tese = []
-    
-#############################################################################################################
-## Desenvolvimento da lógica de execução do Núcleo de processamento do Alpes                                #
-## Aplicação das técnicas de pré-processamento textual a fim de ajudar no processo de comparação e busca    #
-## de textos similares                                                                                      #
-## Técnicas desenvolvidas                                                                                   #
-## 1 - Case folding                                                                                         #
-## 2 - Troca de caracteres acentuados por caracteres não acentuados                                         #
-## 3 - Remoção pontuações                                                                                   #
-## 4 - Remoção de stopwords                                                                                 #
-## 5 - Stemming                                                                                             #
-## 6 - Normalização (falta desenv)                                                                          #
-#############################################################################################################
-
-##############################################################################################################
-## DESENVOLVIMENTO DA FERRAMENTA "GRUPOS DE ARGUMENTAÇÃO" - ARTIGO SBIE 2014
-## UTILIZAÇÃO DO POSICIONAMENTO FINAL PARA CRIAR GRUPOS DE ALUNOS QUE CHEGARAM A CONCLUSÕES SIMILARES
-## AO FINAL DAS INTERAÇÕES DA APDT 
-##############################################################################################################    
+      
 
 #############################################################################################################    
 #Aplicacao de Case Folding
@@ -108,6 +113,11 @@ def clusterArgFinal(idtese):
         
         st_tese.append(string_aux)
 
+#############################################################################################################
+#TESTES!!!!!!!
+
+
+
 
 #############################################################################################################
 #retorno da função - usado na views.py para alimentar o template debate.html
@@ -115,21 +125,33 @@ def clusterArgFinal(idtese):
     return [st_tese, posFinal, sw_tese, aux_usu, st_posFinal]
 
 
-#Experimentos com o Floresta Treebank
-# def simplify_tag(t):
-#     if "+" in t:
-#         return t[t.index("+")+1:]
-#     else:
-#         return t
-#     
-#     twords = floresta.tagged_words()
-#     twords = [(w.lower(), simplify_tag(t)) for (w,t) in twords]
-#     print twords[:10]
-# 
-#     print(' '.join(word + '/' + tag for (word, tag) in twords[:10]))
-# 
-#     psents = floresta.parsed_sents()
-#     print floresta.tagged_sents()
-#     # psents[5].draw()
+#############################################################################################################
+# Normalização dos termos
+# Troca de termos similares por um mesmo termo para auxiliar no cálculo de similaridade
+# Experimentos com o Floresta Treebank
+print floresta.words()
+
+print floresta.tagged_words()
+
+def simplify_tag(t):
+    if "+" in t:
+        return t[t.index("+")+1:]
+    else:
+        return t
+     
+twords = floresta.tagged_words()
+twords = [(w.lower(), simplify_tag(t)) for (w,t) in twords]
+print twords[:10]
+
+print(' '.join(word + '/' + tag for (word, tag) in twords[:10]))
+
+tags = [simplify_tag(tag) for (word,tag) in floresta.tagged_words()]
+fd = FreqDist(tags)
+print fd.keys()[:20]
+
+psents = floresta.parsed_sents()
+print floresta.tagged_sents()
+
+
 
     
