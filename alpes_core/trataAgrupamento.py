c78ac37bdccf185e51b41f9c4803c1e3434b1b66
sabrinapanceri <sabrinapanceri@gmail.com>
2015-11-22 17:56:53 -0200

    novo arquivo com funcionalidades que devem ser implementadas na interface e utilizadas como parâmetros para o funcionamento do sistema

---
+++ alpes_core/trataAgrupamento.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#############################################################################################################
+## 1- COLOCAR NA INTERFACE A OPÇÃO DE DIVIDIR EM QUANTOS GRUPOS O PROFESSOR QUER DIVIDIR OS ALUNOS            ##
+## 2- COLOCAR A OPÇÃO DE ESCOLHAR APLICAR LSA COM O ENVIO DE MATERIAIS OU A PARTIR DAS PRÓPRIAS ARGUMENTAÇÕES ##
+## 3- FAZER "SWITCH" PARA ESSAS OPÇÕES E COLOCAR NESTA FUNÇÃO                                                 ##
+## 4- LINKAR ESSA FUNÇÃO NA VIEW DE TESTE (POSINICIAL1)                                                       ##
+## 
+##
+##
+#############################################################################################################
+def agrupamento():
+    
+    
+    
+    
